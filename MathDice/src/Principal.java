0727265712a0dd01aa372531eeb5b2182343d307
Alfredo <fredypercu@gmail.com>
2015-11-26 11:29:48 +0100

    MathDice

---
+++ MathDice/src/Principal.java
@@ -0,0 +1,24 @@
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class Principal{
+	
+	
+	//La entrada a nuestro programa
+	//Es el método main
+	
+	public static void main(String[] args) {
+		VentanaInicio frame = new VentanaInicio();
+		frame.setVisible(true);
+	
+    }
+	
+	public Principal() {
+		
+	}
+	
+}
