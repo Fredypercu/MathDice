b218cb29a627d601e50275d47566aca6b4c5f196
Pc Alfredo <Pc@192.168.1.47>
2016-02-27 13:31:25 +0100

    P06/Mejorando el Código

--- MathDice V2/src/ventanas/Login.java
+++ MathDice V2/src/ventanas/Login.java
@@ -1,9 +1,11 @@
 package ventanas;
 
 import java.awt.BorderLayout;
+import java.awt.Component;
 import java.awt.EventQueue;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.util.Random;
 
 import javax.swing.ImageIcon;
@@ -24,6 +26,7 @@ public class Login extends JFrame {
 	//Atributos
 	private JPanel contentPane;
 	
+
 	private ImageIcon[] dados3=new ImageIcon[3];
 	private ImageIcon[] dados6=new ImageIcon[6];
 	private ImageIcon[] dados12=new ImageIcon[12];
@@ -32,10 +35,19 @@ public class Login extends JFrame {
 	private JLabel dado4,dado5;
 	private JLabel dado6;
 	
+	
+
+    private int valordado1;
+	private int valordado2;
+	private int valordado3;
+	private int valordado4;
+	private int valordado5;
+
+
 	private Jugador jug;
+
 	
 	//Atributos P05
-
 	private JTextField sumDados;
 	private JButton botonMenos;
 	private Boolean alternando = false; 
@@ -58,6 +70,10 @@ public class Login extends JFrame {
 		//Título Ventana
 		setTitle("Jugada MathDice");
 		
+
+
+
+
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(325, 325, 650, 550);
 		contentPane = new JPanel();
@@ -91,14 +107,14 @@ public class Login extends JFrame {
 		//Int esta entre parentesis porque se esta casteando, como el metodo floor devuelve un double, lo forzamos a que su salida sea un integer
 		
 		//dados3							//Se llama a la clase Math, metodo random y se multiplica por (numMax - numMin +1) + numMin
-		int valordado1 = (int) Math.floor(Math.random()*(2-0+1)+0);
-		int valordado2 = (int) Math.floor(Math.random()*(2-0+1)+0);
+		valordado1 = (int) Math.floor(Math.random()*(2-0+1)+0);
+		valordado2 = (int) Math.floor(Math.random()*(2-0+1)+0);
 		
 		
 		//dados6
-		int valordado3 = (int) Math.floor(Math.random()*(5-0+1)+0);
-		int valordado4 = (int) Math.floor(Math.random()*(5-0+1)+0);
-		int valordado5 = (int) Math.floor(Math.random()*(5-0+1)+0);
+		valordado3 = (int) Math.floor(Math.random()*(5-0+1)+0);
+		valordado4 = (int) Math.floor(Math.random()*(5-0+1)+0);
+		valordado5 = (int) Math.floor(Math.random()*(5-0+1)+0);
 		
 		//dodecaedro
 		int valordado6 = (int) Math.floor(Math.random()*(11-0+1)+0);
@@ -110,174 +126,49 @@ public class Login extends JFrame {
 		//SetIcon llama a la posicion valordado1 del array dados3 y cuando lo llama lo transforma en icono
 		dado1.setIcon(dados3[valordado1]);
 		dado1.setBounds(10, 11, 125, 112);
+		dado1.setText(String.valueOf(valordado1)); //metemos el valor del dado en el objeto tipo dado para recogerlo en la clase inerclass
 		contentPane.add(dado1);
 		
 		
-		dado1.addMouseListener(
+
+		dado1.addMouseListener(new ListenerDados());
 				
-				//Esta es la Clase que tiene el metodo para que se ejecute la accion del raton
-				new MouseAdapter() { 
-					MouseAdapter referencia=this;
-					//Esta es el metodo para que se ejecute la accion del raton
-					//Void porque no va a devolver nada
-					public void mouseClicked(MouseEvent arg0) {
-						//Comprobacion de que alternando esta en false, en este caso se ejecutará el codigo de dentro y se cambiara 
-						//a true para que no podamos pinchar otra vez el mismo tipo de boton (dados)
-						if (alternando==false){
-							dado1.setEnabled(false);
-							String aux=sumDados.getText(); //<- Se coge el texto que exista para actualizar sin perder los datos ya introducidos
-							String aux2=aux + String.valueOf(valordado1+1); //+1 porque el contador empieza ya con valor 0
-							sumDados.setText(aux2);
-							dado1.removeMouseListener(referencia);
-							alternando=true;
-							
-							//Para primera eleccion pulsada
-							if(resultadoFinal == 0) {
-								resultadoFinal = valordado1+1;
-							//Para las demas elecciones
-							}else{
-								if(controladorOperacion == 1){//==1 suma
-									resultadoFinal = resultadoFinal + (valordado1+1);
-								}else if (controladorOperacion == 0){//==0 resta
-									resultadoFinal = resultadoFinal - (valordado1+1);
-								}
-							}
-						}
-					}
-				}
-			);
-		
+				
 		dado2 = new JLabel();
 		dado2.setIcon(dados3[valordado2]);
+		dado2.setText(String.valueOf(valordado2));
 		dado2.setBounds(145, 11, 125, 112);
 		contentPane.add(dado2);
 		
-		dado2.addMouseListener(
-				//Esta es la Clase que tiene el metodo para que se ejecute la accion del raton
-				new MouseAdapter() {
-					MouseAdapter referencia=this;
-					//Esta es el metodo para que se ejecute la accion del raton
-					//Void porque no va a devolver nada
-					public void mouseClicked(MouseEvent arg0) {
-						if (alternando==false){
-							dado2.setEnabled(false);
-							String aux=sumDados.getText(); //<- Se coge el texto que exista para actualizar sin perder los datos ya introducidos
-							String aux2=aux + String.valueOf(valordado2+1);
-							sumDados.setText(aux2);
-							dado2.removeMouseListener(referencia);
-							alternando=true;
-							if(resultadoFinal == 0) {
-								resultadoFinal = valordado2+1;
-							}else{
-								if(controladorOperacion == 1){
-									resultadoFinal = resultadoFinal + (valordado2+1);
-								}else if (controladorOperacion == 0){
-									resultadoFinal = resultadoFinal - (valordado2+1);
-								}
-							}
-						}
-					}
-				}
-			);
-		
+		dado2.addMouseListener(new ListenerDados());
+
+			
 		dado3 = new JLabel();
 		dado3.setIcon(dados6[valordado3]);
+		dado3.setText(String.valueOf(valordado3));
 		dado3.setBounds(10, 132, 125, 112);
 		contentPane.add(dado3);
 		
-		dado3.addMouseListener(
-				//Esta es la Clase que tiene el metodo para que se ejecute la accion del raton
-				new MouseAdapter() { 
-					MouseAdapter referencia=this;
-					//Esta es el metodo para que se ejecute la accion del raton
-					//Void porque no va a devolver nada
-					public void mouseClicked(MouseEvent arg0) {
-						if (alternando==false){
-							dado3.setEnabled(false);
-							String aux=sumDados.getText(); //<- Se coge el texto que exista para actualizar sin perder los datos ya introducidos
-							String aux2=aux + String.valueOf(valordado3+1);
-							sumDados.setText(aux2);
-							dado3.removeMouseListener(referencia);
-							alternando=true;
-							if(resultadoFinal == 0) {
-								resultadoFinal = valordado3+1;
-							}else{
-								if(controladorOperacion == 1){
-									resultadoFinal = resultadoFinal + (valordado3+1);
-								}else if (controladorOperacion == 0){
-									resultadoFinal = resultadoFinal - (valordado3+1);
-								}
-							}
-						}
-					}
-				}
-			);
+		dado3.addMouseListener(new ListenerDados());
+				
 		
 		dado4 = new JLabel();
 		dado4.setIcon(dados6[valordado4]);
+		dado4.setText(String.valueOf(valordado4));
 		dado4.setBounds(145, 132, 125, 112);
 		contentPane.add(dado4);
 		
-		dado4.addMouseListener(
-				//Esta es la Clase que tiene el metodo para que se ejecute la accion del raton
-				new MouseAdapter() { 
-					MouseAdapter referencia=this;
-					//Esta es el metodo para que se ejecute la accion del raton
-					//Void porque no va a devolver nada
-					public void mouseClicked(MouseEvent arg0) {
-						if (alternando==false){
-							dado4.setEnabled(false);
-							String aux=sumDados.getText(); //<- Se coge el texto que exista para actualizar sin perder los datos ya introducidos
-							String aux2=aux + String.valueOf(valordado4+1);
-							sumDados.setText(aux2);
-							dado4.removeMouseListener(referencia);
-							alternando=true;
-							if(resultadoFinal == 0) {
-								resultadoFinal = valordado4+1;
-							}else{
-								if(controladorOperacion == 1){
-									resultadoFinal = resultadoFinal + (valordado4+1);
-								}else if (controladorOperacion == 0){
-									resultadoFinal = resultadoFinal - (valordado4+1);
-								}
-							}
-						}
-					}
-				}
-			);
+		dado4.addMouseListener(new ListenerDados());
+				
 		
 		dado5 = new JLabel();
 		dado5.setIcon(dados6[valordado5]);
+		dado5.setText(String.valueOf(valordado5));
 		dado5.setBounds(280, 132, 125, 112);
 		contentPane.add(dado5);
 		
-		dado5.addMouseListener(
-				//Esta es la Clase que tiene el metodo para que se ejecute la accion del raton
-				new MouseAdapter() { 
-					MouseAdapter referencia=this;
-					//Esta es el metodo para que se ejecute la accion del raton
-					//Void porque no va a devolver nada
-					public void mouseClicked(MouseEvent arg0) {
-						if (alternando==false){
-						dado5.setEnabled(false);
-						String aux=sumDados.getText(); //<- Se coge el texto que exista para actualizar sin perder los datos ya introducidos
-						String aux2=aux + String.valueOf(valordado5+1);
-						sumDados.setText(aux2);
-						dado5.removeMouseListener(referencia);
-						alternando=true;
-						if(resultadoFinal == 0) {
-							resultadoFinal = valordado5+1;
-						}else{
-							if(controladorOperacion == 1){
-								resultadoFinal = resultadoFinal + (valordado5+1);
-							}else if (controladorOperacion == 0){
-								resultadoFinal = resultadoFinal - (valordado5+1);
-							}
-						}
-						}
-					}
-				}
-			);
+		dado5.addMouseListener(new ListenerDados());
+
 		
 		dado6 = new JLabel();
 		dado6.setIcon(dados12[valordado6]);
@@ -290,6 +181,7 @@ public class Login extends JFrame {
 		sumDados.setBounds(435, 331, 130, 48);
 		contentPane.add(sumDados);
 		sumDados.setColumns(10);
+
 
 		//Boton de suma
 		JButton botonMas = new JButton("+");
@@ -356,6 +248,8 @@ public class Login extends JFrame {
 		contentPane.add(botonMathdice);
 
 
+
+
 		//Jlabel que muestra si has ganado o no
 		resultadoenTexto = new JLabel();
 		resultadoenTexto.setBounds(357, 390, 141, 48);
@@ -379,6 +273,77 @@ public class Login extends JFrame {
 		botonRelanzar.setEnabled(false);
 		botonRelanzar.setBounds(10, 404, 169, 23);
 		contentPane.add(botonRelanzar);
+		
+		
+	}
+	
+	private class ListenerDados implements MouseListener {
+		
+		ListenerDados ref=this;
+		
+		@Override
+		public void mouseClicked(MouseEvent e) {
+			
+			//Para saber que dado estamos utilizando
+			JLabel j=(JLabel)e.getSource();
+			
+			//Comprobacion de que alternando esta en false, en este caso se ejecutará el codigo de dentro y se cambiara 
+			//a true para que no podamos pinchar otra vez el mismo tipo de boton (dados)
+			if (alternando==false){
+				
+				//Recogemos el valor en forma de integer de la tirada del dado (lo que vale el dado)
+				int valorDado=Integer.valueOf(j.getText());
+				j.setEnabled(false);
+				String aux=sumDados.getText(); //<- Se coge el texto que exista para actualizar sin perder los datos ya introducidos
+				String aux2=aux + String.valueOf(valorDado+1); //+1 porque el contador empieza ya con valor 0
+				sumDados.setText(aux2);
+				
+				//Eliminamos el MouseListener referenciado al objeto para que no se puede clickar 2 veces
+				j.removeMouseListener(ref);
+				
+				alternando=true;
+				
+				
+				
+				//Para primera eleccion pulsada
+				if(resultadoFinal == 0) {
+					resultadoFinal = valorDado+1;
+				//Para las demas elecciones
+				}else{
+					if(controladorOperacion == 1){//==1 suma
+						resultadoFinal = resultadoFinal + (valorDado+1);
+					}else if (controladorOperacion == 0){//==0 resta
+						resultadoFinal = resultadoFinal - (valorDado+1);
+					}
+				}
+			}
+			
+		}
+		
+
+		@Override
+		public void mouseEntered(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mouseExited(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mousePressed(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mouseReleased(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
 	
 	}
 }
